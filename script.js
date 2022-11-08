fa3bf4e8c5e8c077e8c2b68e62df6ca5767820f7
Gabriel Boniolo <gabrielboniolo@gmail.com>
2022-11-07 23:14:07 -0300

    periodoDoDia

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 function carregar() {
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
+    var titulo = document.getElementById('titulo')
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if(hora>=4 && hora<12) {
+        titulo.innerHTML = 'Bom dia!'
         img.src = 'imagens/foto manha.jpg'
         document.body.style.background = '#8e985a'
     }
 
     else if(hora>=12 && hora<18) {
+        titulo.innerHTML = 'Boa tarde!'
         img.src = 'imagens/foto tarde.jpg'
         document.body.style.background = '#9ba7b7'
     }
 
     else{
+        titulo.innerHTML = 'Boa noite!'        
            img.src = 'imagens/foto noite.jpg' 
            document.body.style.background = '#294f64'
         }
